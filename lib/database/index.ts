f9382cbafff06d9463c6442aed66dee66cdc503d
nyeinminhtet <nyeinmg904@gmail.com>
2023-12-20 22:49:38 +0630

    feat:homeAndMongodb-setup

---
+++ lib/database/index.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.connn;
+
+  if (!MONGODB_URL) throw new Error("MONGODB URL is missing!");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "mandalyEvents",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
